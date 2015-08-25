93e4ac93716c87b4d50e381ddb897cd553b8b6ba
Richard Li <xpp@me.com>
2015-08-25 11:38:07 +0800

    feat(email-CRUD): user can create and send emails after login (no scheduling yet)

---
+++ app/mailers/main_mailer.rb
@@ -0,0 +1,11 @@
+class MainMailer < ApplicationMailer
+  def send_email(email)
+    @email = email
+    mail(to: @email.to, subject: @email.title, from: @email.from) do |format|
+      format.html { render html: @email.content }
+      format.text { render plain: @email.content }
+    end
+
+
+  end
+end
